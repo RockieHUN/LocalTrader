4bb2219b887ebd562cce403dc5152559088a5142
Császár Ákos <57495891+RockieHUN@users.noreply.github.com>
2020-12-21 22:10:13 +0200

    Login UI

---
+++ app/src/main/java/com/example/localtrader/authentication/LoginFragment.kt
@@ -0,0 +1,38 @@
+package com.example.localtrader.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.localtrader.R
+import com.example.localtrader.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding : FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+
+}
